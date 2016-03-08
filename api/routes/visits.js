5f9b060e4e68b281fd35e0d8d5ec29b22f510bad
Vitalii Savchuk <esvit666@gmail.com>
2016-03-08 14:46:30 +0200

    add analytics

---
+++ api/routes/visits.js
@@ -0,0 +1,134 @@
+'use strict';
+
+var router = require('express').Router(),
+  mongoose = require('mongoose'),
+  moment = require('moment'),
+  _ = require('lodash'),
+  async = require('async');
+
+var getPagesStat = function(app, siteId, startDate, endDate, next) {
+  var range1 = moment.range(startDate, endDate),
+    diff = range1.diff('days'),
+    startDate2 = moment(startDate).subtract(diff, 'day'),
+    endDate2 = moment(endDate).subtract(diff, 'day');
+
+  siteId = new mongoose.Types.ObjectId(siteId);
+  async.auto({
+    'stats1': function(next) {
+      app.models.visitStatistics.aggregate([
+        {
+          $match: {
+            'site._id': siteId,
+            date: { $gte: startDate.toDate(), $lt: endDate.toDate() }
+          }
+        },
+        {
+          $group: {
+            _id: { pageId: '$page._id' },
+            sessions: { $sum: "$sessions" } ,
+            users: { $sum: "$users" }
+          }
+        }
+      ], next);
+    },
+    'stats2': function(next) {
+      app.models.visitStatistics.aggregate([
+        {
+          $match: {
+            'site._id': siteId,
+            date: { $gte: startDate2.toDate(), $lt: endDate2.toDate() }
+          }
+        },
+        {
+          $group: {
+            _id: { pageId: '$page._id' },
+            sessions: { $sum: "$sessions" } ,
+            users: { $sum: "$users" }
+          }
+        }
+      ], next);
+    },
+    'diff': ['stats1', 'stats2', function(next, data) {
+      var pages = {};
+      _.each(data.stats1, function(item) {
+        pages[item._id.pageId] = {
+          users: item.users,
+          sessions: item.sessions
+        }
+      });
+      _.each(data.stats2, function(item) {
+        if (pages[item._id.pageId]) {
+          pages[item._id.pageId].oldUsers = item.users;
+          pages[item._id.pageId].oldSessions = item.sessions;
+        }
+      });
+      next(null, pages)
+    }],
+    'pages': ['diff', function(next, data) {
+      var keys = _.keys(data.diff);
+      app.models.pages.find({ _id: { $in: keys } }, next);
+    }]
+  }, function(err, data) {
+    if (err) { return next(err); }
+
+    var result = _.map(data.pages, function(item) {
+      var stat = data.diff[item._id],
+        page = item.toObject();
+      page.users = stat.users;
+      page.sessions = stat.sessions;
+      page.oldUsers = stat.oldUsers;
+      page.oldSessions = stat.oldSessions;
+      if (stat.sessions && stat.oldSessions) {
+        page.diffSessions = stat.sessions - stat.oldSessions;
+      }
+      if (stat.users && stat.oldUsers) {
+        page.diffUsers = stat.users - stat.oldUsers;
+      }
+      return page;
+    });
+    next(null, result);
+  });
+};
+
+router.get('/grown-up', function (req, res, next) {
+  var endDate = moment.min(moment(), moment(req.query.date_to, 'YYYY-MM-DD')),
+    startDate = moment.min(moment(), moment(req.query.date_from, 'YYYY-MM-DD')),
+    siteId = req.query['site._id'];
+
+  getPagesStat(req.app, siteId, startDate, endDate, function(err, result) {
+    if (err) { return next(err); }
+
+    result = _.filter(result, function(item) {
+      return item.diffUsers > 0;
+    });
+    result = _.sortBy(result, function(item) {
+      return -item.diffUsers;
+    });
+    result = _.take(result, 10);
+
+    res.json(result)
+  });
+});
+
+router.get('/drop-in', function (req, res, next) {
+  var endDate = moment.min(moment(), moment(req.query.date_to, 'YYYY-MM-DD')),
+    startDate = moment.min(moment(), moment(req.query.date_from, 'YYYY-MM-DD')),
+    siteId = req.query['site._id'];
+
+  getPagesStat(req.app, siteId, startDate, endDate, function(err, result) {
+    if (err) { return next(err); }
+
+    result = _.filter(result, function(item) {
+      return item.diffUsers <= 0;
+    });
+    result = _.sortBy(result, function(item) {
+      return item.diffUsers;
+    });
+    result = _.take(result, 10);
+
+    res.json(result)
+  });
+});
+
+
+module.exports = router;
