f62275f5e8f91955b7527b05d1dc74afabc8a8e1
Polluxx <yuriy.kosakivsky@gmail.com>
2016-04-26 10:08:41 +0300

    add analytics page stat

---
+++ api/tasks/pages.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var moment = require('moment');
+
+exports['pages.scan'] = function(app, message, next) {
+
+  async.auto({
+    projects: function () {
+
+    },
+    pages: ['projects', function(data) {
+      
+    }]
+  });
+
+}
--- app/modules/analytics/projects/controllers/apPagesViewCtrl.js
+++ app/modules/analytics/projects/controllers/apPagesViewCtrl.js
@@ -1,8 +1,15 @@
 export default
 /*@ngInject*/
 function($scope, item, project, ngAnalyticsService, aSiteModel, $http, NgTableParams) {
-  console.log(item);
+  /* tmp */
   console.log(project);
+  item = item.data[0];
+
+  item.url = [item.category.parentAlias, item.category.alias, item.alias].join("/");
+  console.log(item);
+  /* tmp */
+
+
   $scope.item = item;
   $scope.project = project;
 
