6ec67d970018909de1976e58a520e2e648894471
Polluxx <yuriy.kosakivsky@gmail.com>
2016-05-05 15:31:14 +0300

    add analytics page stat

--- app/modules/analytics/projects/controllers/apProjectPagesCtrl.js
+++ app/modules/analytics/projects/controllers/apProjectPagesCtrl.js
@@ -9,18 +9,31 @@ function ($scope, item, ngAnalyticsService, aSiteModel, aPageModel, NgTableParam
   };
 
   $scope.item = item;
+  $scope.total = 0;
 
   $scope.tableParams = new NgTableParams({
     page: 1,
-    count: $scope.counter || 100
+    count: $scope.counter || 10
   }, {
     getData: function (params) {
       return aPageModel.query({page: params.page(), perPage: params.count(), siteId: item._id}, function (resp, headers) {
         $scope.counter = params.count();
         $scope.pages = resp;
-        params.total(headers()['x-total-count']);
+        $scope.total = parseInt(headers('x-total-count'));
+        params.total($scope.total);
         return $scope.pages;
       });
+    },
+    paginationMaxBlocks: 10,
+    paginationMinBlocks: 2
+  });
+
+  var total = $scope.tableParams.total();
+  $scope.$watch('total', () => {
+    if($scope.total !== total) {
+      $scope.tableParams.total($scope.total);
+      total = $scope.tableParams.total();
+      $scope.tableParams.reload();
     }
   });
 }
